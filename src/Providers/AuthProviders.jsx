7e1da0b1965125754925223b79426802885dc017
forhadhossen1 <mr.forhad1030@gamil.com>
2023-12-21 01:42:36 +0600

    set router and firebase authentication system

---
+++ src/Providers/AuthProviders.jsx
@@ -0,0 +1,17 @@
+import { createContext } from "react";
+
+export const AuthContext = createContext(null);
+
+const AuthProviders = ({ children }) => {
+
+    const authInfo = {
+
+    }
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProviders;
